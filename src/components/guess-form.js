a85746a8968b8f2566c226e99bb2979618443548
Vernon Mensah <vernonmensah@gmail.com>
2018-01-26 10:56:24 -0500

    Slight changes!

---
+++ src/components/guess-form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './guess-form.css';
+
+export default class GuessForm extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            value: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+() =>
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        console.log(this.state.value);
+    }
+
+    handleOnSubmit(event) {
+
+      event.preventDefault();
+      this.props.feedback(this.state.value);
+      this.setState({value: ''});
+
+    }
+
+
+    render() {
+    return (
+        <form onSubmit={(e)=> {this.handleOnSubmit(e)}}>
+            <input r={this.state.value} onChange={this.handleChange} type="text" name="userGuess" id="userGuess"
+                className="text" maxLength="3" autoComplete="off"
+                placeholder="Enter your Guess" required />
+            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+        </form>
+    );
+    }
+};
